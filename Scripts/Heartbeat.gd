5e215db87985151a95fdbdff1917bbcbc29b7f54
Scubadoo2 <scubadoo2@rocketmail.com>
2021-02-15 11:04:22 -0800

    Made it easier to reconize where the beat is.

--- Scripts/Heartbeat.gd
+++ Scripts/Heartbeat.gd
@@ -1,16 +1,24 @@
 extends Control
 
 #Heartbeat variables we set ourselves
-var forgive = .1 # amount off a person can be from the beat
+var forgive = .2 # amount off a person can be from the beat
 
 #Quick reference to objects
 onready var timer = $Timer
 onready var meter = $Meter
-onready var meterPlace  = $Meter/Place
 onready var heart = $Heart
+onready var meterCenter  = $Meter/Center
+
+#References to the bars
+onready var meterRO  = $Meter/PlaceRO
+onready var meterRI  = $Meter/PlaceRI
+onready var meterLO  = $Meter/PlaceLO
+onready var meterLI  = $Meter/PlaceLI
 
 #Placement to lessen calculation time
 var yPlacement
+var halfway
+var halfWidth
 
 #Signals
 signal heartbeat()
@@ -18,7 +26,10 @@ signal heartbeat()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	yPlacement = (meter.rect_size.y/2) - (meterPlace.rect_size.y/2)
+	yPlacement = (meter.rect_size.y/2) - (meterCenter.rect_size.y/2)
+	halfway = meter.rect_size.x/2
+	halfWidth = meterCenter.rect_size.x/2
+	#barWidth = meterCenter.rect_size.x
 	
 	# Make it acutal timing for both-sided check
 	forgive = forgive/2
@@ -29,8 +40,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var timeLeft = 1-(timer.time_left / timer.wait_time)
+	var distance = (meter.rect_size.x * timeLeft)/4
 	
-	meterPlace.set_position(Vector2((meter.rect_size.x * timeLeft),yPlacement))
+	#meterPlace.set_position(Vector2((meter.rect_size.x * timeLeft),yPlacement))
+	meterRO.set_position(Vector2(halfway*2 - distance - halfWidth,yPlacement))
+	meterRI.set_position(Vector2(halfway*1.5 - distance - halfWidth,yPlacement))
+	
+	meterLO.set_position(Vector2(distance - halfWidth,yPlacement))
+	meterLI.set_position(Vector2(halfway/2 + distance - halfWidth,yPlacement))
 	
 func IsInBeat():
 	if(timer.time_left < forgive || (timer.wait_time - timer.time_left) < forgive):
