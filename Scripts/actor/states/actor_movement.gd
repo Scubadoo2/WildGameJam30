7e411c47a809e2603a4033673a1abf05c10bd118
Jonas <jonas_munz@outlook.com>
2021-02-19 13:43:35 -0600

    Added the South direction sprite

--- Scripts/actor/states/actor_movement.gd
+++ Scripts/actor/states/actor_movement.gd
@@ -4,12 +4,18 @@ var actor: KinematicBody2D
 var animation: AnimatedSprite
 var debug_info: Label
 
+export (bool) var enable_footstep: bool = false
+export (float) var footstep_freq = 0.9
+# footsteps
+var left_step: bool
+var right_step: bool
+
 func enter():
 	actor.animation_mode.travel("Walking")
 	debug_info.text = "Movement"
 	
 func exit():
-	pass
+	$Timer.stop()
 	
 func handle_input(event):
 	pass
@@ -19,6 +25,7 @@ func tick(delta):
 	actor.animation_tree.set("parameters/Walking/blend_position", actor.direction)
 	if actor.direction == Vector2.ZERO:
 		return "Idle"
+	footsteps()
 	move(delta)
 	
 func setup_state():
@@ -29,3 +36,20 @@ func setup_state():
 func move(delta):
 	actor.animation_mode.travel("Walking")
 	actor.move_and_slide(actor.direction.normalized() * actor.speed)
+
+func footsteps():
+	if enable_footstep:
+		# Begin footsteps
+		if left_step == false and right_step == false:
+			$Timer.start(footstep_freq)
+			left_step = true
+		# Begin right step
+		elif left_step and $Timer.is_stopped():
+			left_step = false
+			right_step = true
+			$Timer.start(footstep_freq)
+		# Begin left step
+		elif right_step and $Timer.is_stopped():
+			right_step = false
+			left_step = true
+			$Timer.start(footstep_freq)
